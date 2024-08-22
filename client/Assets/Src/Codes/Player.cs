9c0f01c112689cc1e3abf6caf17907a83b3f1d27
diddntjd99 <diddntjd99@gmail.com>
2024-08-22 21:39:08 +0900

    ♻️ refactor : 사용하지 않는 Debug.Log 제거

--- client/Assets/Src/Codes/Player.cs
+++ client/Assets/Src/Codes/Player.cs
@@ -427,14 +427,12 @@ public class Player : MonoBehaviour
         {
             yield return new WaitForSeconds(zSkill_CoolTime);
             isZSkill = true;
-            Debug.Log("isZSkill : " + isZSkill);
         }
 
         else
         {
             yield return new WaitForSeconds(xSkill_CoolTime);
             isXSkill = true;
-            Debug.Log("isXSkill : " + isXSkill);
         }
     }
 
